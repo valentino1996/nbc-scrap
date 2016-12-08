98292cd8a277b9654a88e0cd16adbb789fb6b85b
valentino1996 <valentino.isufi1@gmail.com>
2016-12-08 15:21:24 +0100

    uploading changes

--- index.js
+++ index.js
@@ -2,76 +2,81 @@ var express = require("express"),
 	mongoose = require("mongoose"),
 	bodyParser = require("body-parser"),
 	passport = require("passport"),
-	passportTwitter = require("passport-twitter").Strategy,
+	TwitterStrategy = require("passport-twitter").Strategy,
 	Xray = require("x-ray");
 	
 var app = express();
 var x = Xray();
 
-app.use(express.static("public"));
+//app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(passport.initialize());
 app.use(passport.session());
 
+var user="";
+var token = '2538355556-GeEuOWBlO2wo7vi6zHq9nEOrfdGRIkZQT1wcW1P';
+var tokenSecret = 'fuSWRwwzBRVSQrskxEwSHIZoFBc0PJUydlkz94rLDyA9T';
 var idName ="";
 var obj = {
+	
+	data:[
 		
-		one:{
+		{
 			newsLink: "html body main " + idName + "#top-stories-section div div div div div div div a@href",
 			img: "html body main " + idName + "#top-stories-section div div div div div div img@src",
 			title: "html body main " + idName + "#top-stories-section div div div div div div div a div img@alt"
 		},
 		
-		two:{
+		{
 			newsLink: "html body main " + idName + "#top-stories-section .col-sm-12:nth-of-type(1) .visible-lg-block a@href",
 			img: "html body main " + idName + "#top-stories-section .col-sm-12:nth-of-type(1) .visible-lg-block img@src",
 			title: "html body main " + idName + "#top-stories-section .col-sm-12:nth-of-type(1) .visible-lg-block img@alt"
 		},
 		
-		three:{
+		{
 			newsLink: "html body main " + idName + "#top-stories-section .col-sm-12:nth-of-type(2) .visible-lg-block a@href",
 			img: "html body main " + idName + "#top-stories-section .col-sm-12:nth-of-type(2) .visible-lg-block img@src",
 			title: "html body main " + idName + "#top-stories-section .col-sm-12:nth-of-type(2) .visible-lg-block img@alt"
 		},
 		
-		four:{
+		{
 			newsLink: "html body main " + idName + "#top-stories-section .col-lg-4 .col-md-lg-6:nth-of-type(1) .story-link:nth-of-type(1) a@href",
 			img: "html body main " + idName + "#top-stories-section .col-lg-4 .col-md-lg-6:nth-of-type(1) .story-link:nth-of-type(1) img@data-original",
 			title: "html body main " + idName + "#top-stories-section .col-lg-4 .col-md-lg-6:nth-of-type(1) .story-link:nth-of-type(1) img@alt"
 		},
-		
-		five:{
+
+		{
 			newsLink: "html body main " + idName + "#top-stories-section .col-lg-4 .col-md-lg-6:nth-of-type(1) .story-link:nth-of-type(2) a@href",
 			img: "html body main " + idName + "#top-stories-section .col-lg-4 .col-md-lg-6:nth-of-type(1) .story-link:nth-of-type(2) img@data-original",
 			title: "html body main " + idName + "#top-stories-section .col-lg-4 .col-md-lg-6:nth-of-type(1) .story-link:nth-of-type(2) img@alt"
-			},
+		},
 		
-		six:{
+		{
 			newsLink: "html body main " + idName + "#top-stories-section .col-lg-4 .col-md-lg-6:nth-of-type(1) .story-link:nth-of-type(3) a@href",
 			img: "html body main " + idName + "#top-stories-section .col-lg-4 .col-md-lg-6:nth-of-type(1) .story-link:nth-of-type(3) img@data-original",
 			title: "html body main " + idName + "#top-stories-section .col-lg-4 .col-md-lg-6:nth-of-type(1) .story-link:nth-of-type(3) img@alt"
-			},
+		},
 		
-		seven:{
+		{
 			newsLink: "html body main " + idName + "#top-stories-section .col-lg-4 .col-md-lg-6:nth-of-type(2) .story-link:nth-of-type(1) a@href",
 			img: "html body main " + idName + "#top-stories-section .col-lg-4 .col-md-lg-6:nth-of-type(2) .story-link:nth-of-type(1) img@data-original",
 			title: "html body main " + idName + "#top-stories-section .col-lg-4 .col-md-lg-6:nth-of-type(2) .story-link:nth-of-type(1) img@alt"
-			},
+		},
 		
-		eight:{
+		{
 			newsLink: "html body main " + idName + "#top-stories-section .col-lg-4 .col-md-lg-6:nth-of-type(2) .story-link:nth-of-type(2) a@href",
 			img: "html body main " + idName + "#top-stories-section .col-lg-4 .col-md-lg-6:nth-of-type(2) .story-link:nth-of-type(2) img@data-original",
 			title: "html body main " + idName + "#top-stories-section .col-lg-4 .col-md-lg-6:nth-of-type(2) .story-link:nth-of-type(2) img@alt"
-			},
+		},
 		
-		nine:{
+		{
 			newsLink: "html body main " + idName + "#top-stories-section .col-lg-4 .col-md-lg-6:nth-of-type(2) .story-link:nth-of-type(3) a@href",
 			img: "html body main " + idName + "#top-stories-section .col-lg-4 .col-md-lg-6:nth-of-type(2) .story-link:nth-of-type(3) img@data-original",
 			title: "html body main " + idName + "#top-stories-section .col-lg-4 .col-md-lg-6:nth-of-type(2) .story-link:nth-of-type(3) img@alt"
-			}
+		}
 		
-	}
+	]}
 
 /*
 mongoose.connect("mongodb://test:test@ds053156.mlab.com:53156/mongodb-test-valentino", function (err) {
@@ -97,6 +102,33 @@ mongoose.connection.once("open", function(err){
 		
 	}
 	*/
+	passport.use(new TwitterStrategy({
+		consumerKey: '7yqe1MKJmAHBBV97lro32cZi9',
+		consumerSecret: 'F573BNj9Daj1ohAjnT2Q79EUPwXv0c14r2gbsQbuX23Ct6iL4E',
+		callbackURL: "http://127.0.0.1:8080/auth/twitter/callback"
+	},
+	function(token, tokenSecret, profile, done) {
+		
+			console.log(profile);
+			console.log(profile.id);
+			user=profile.id;
+			
+			passport.serializeUser(function(user, done) {
+				done(null, user);
+			});
+
+			passport.deserializeUser(function(user, done) {
+				done(null, user);
+			});
+			
+			return done(null, profile.id);
+			
+		}
+	));
+	
+	app.get("/", function(req, res){
+		
+	});
 
 app.get("/health", function(req, res){
 	
@@ -201,6 +233,18 @@ app.get("/us", function(req, res){
 		res.json(code);
 	});
 });
+
+	
+	app.get('/auth/twitter', passport.authenticate('twitter'));
+	
+	app.get("/login", function(req, res){
+		res.send("twitter authentication went wrong");
+	})
+
+	app.get('/auth/twitter/callback',
+		passport.authenticate('twitter', { successRedirect: '/',
+                                     failureRedirect: '/login' }));
+	
 
 //});
 app.listen(process.env.PORT||8080);
